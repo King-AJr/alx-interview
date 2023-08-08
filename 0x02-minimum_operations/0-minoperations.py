95778753ac5a00eb720efc378c3e23d6479c3bb6
King-AJr <talk2king.aj@gmail.com>
2023-08-08 16:36:41 +0100

    calculate the fewest number of operations needed to result in exactly n H characters in the file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single
+character H. Your text editor can execute
+only two operations in this file: Copy All
+and Paste
+Given a minOperations(n) calculates
+the fewest number of operations needed
+to result in exactly n H characters in
+the file.
+"""
+
+def minOperations(n: int) -> int:
+    """calculates
+    the fewest number of operations needed
+    to result in exactly n H characters in
+    the file."""
+    i: int = 2
+    result: int = 0
+    if n < 2:
+        return 0
+    while result < n:
+        if i % 2 == 0:
+            result = result + (i - 1)
+        else:
+            result = result + i
+        i += 1
+    return i
