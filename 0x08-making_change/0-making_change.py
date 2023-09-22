77e955d840535713a189ecfba51d07d4b4a272fc
King-AJr <talk2king.aj@gmail.com>
2023-09-22 02:21:14 +0100

    conform to pycodestyle

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,10 +3,12 @@
 Change making module.
 """
 
+
 def makeChange(coins: int, total: int) -> int:
     """
     Determines the fewest number of coins needed to meet a given
-    amount (total_amount) when given a pile of coins of different values (coins).
+    amount (total_amount) when given a pile of coins of different
+      values (coins).
     :param coins: List of coin denominations (e.g., [1, 5, 10, 25] for cents)
     :param total_amount: The target amount to make change for
     :return: The minimum number of coins needed, or -1 if change cannot be made
