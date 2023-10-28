2051e51b51b4ed4e07804fc64faa2d449294336a
King-AJr <talk2king.aj@gmail.com>
2023-10-28 17:18:49 +0100

    rewrite stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,82 +1,92 @@
 #!/usr/bin/python3
-"""HTTP Request Log Parser
+'''A script for parsing HTTP request logs.
+'''
+import re
 
-This script parses and analyzes HTTP request logs,
-extracting information about each log entry and
-generating statistics on status codes and total file size.
+def extract_request_info(log_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    # Regular expressions to match different parts of the log line
+    patterns = (
+        r'\s*(?P<ip>\S+)\s*',  # IP address
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',  # Date and time
+        r'\s*"(?P<request>[^"]*)"\s*',  # Request details
+        r'\s*(?P<status_code>\S+)',  # Status code
+        r'\s*(?P<file_size>\d+)'  # File size
+    )
+    
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    
+    # Construct a log format using the patterns
+    log_format = '{}\\-{}{}{}{}\\s*'.format(patterns[0], patterns[1], patterns[2], patterns[3], patterns[4])
+    
+    # Attempt to match the log line with the log format
+    match = re.fullmatch(log_format, log_line)
+    
+    # If a match is found, extract status code and file size
+    if match is not None:
+        status_code = match.group('status_code')
+        file_size = int(match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
-Usage:
-1. Provide input by reading log entries from standard input.
-2. The script will accumulate statistics and display
-them in real-time every 10 log entries.
+def print_http_stats(total_file_size, status_code_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
+    print('Total File Size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_code_stats.keys()):
+        num = status_code_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
-Example Log Entry:
-192.168.1.1 [2023-10-28 14:30:45.123] "GET /page.html" 200 1234
+def update_metrics(log_line, total_file_size, status_code_stats):
+    '''Updates the metrics from a given HTTP request log.
 
-Log Entry Structure:
-- IP address
-- Date and time
-- Request details
-- HTTP status code
-- File size
-"""
+    Args:
+        log_line (str): The line of input from which to retrieve the metrics.
 
-import sys
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_request_info(log_line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_code_stats.keys():
+        status_code_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
 
-# Define a dictionary to store the count of status codes
-status_code_counts = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
+def run_log_parser():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_code_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
+    
+    try:
+        while True:
+            log_line = input()
+            total_file_size = update_metrics(
+                log_line,
+                total_file_size,
+                status_code_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_http_stats(total_file_size, status_code_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_http_stats(total_file_size, status_code_stats)
 
-total_file_size = 0
-line_count = 0  # Keep track of the number of lines processed
-
-try:
-    for line in sys.stdin:
-        # Split the input line by spaces
-        line_parts = line.split(" ")
-
-        # Check if the line contains enough elements
-        if len(line_parts) > 4:
-            status_code = line_parts[-2]
-            file_size = int(line_parts[-1])
-
-            # Check if the status code exists in the
-            # dictionary and increment its count
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
-
-            # Update the total file size
-            total_file_size += file_size
-
-            # Update the line count
-            line_count += 1
-
-        # Print status code counts and total file size every 10 lines
-        if line_count == 10:
-            line_count = 0  # Reset the line count
-
-            # Print the total file size
-            print("Total File Size: {}".format(total_file_size))
-
-            # Print out status code counts
-            for code, count in sorted(status_code_counts.items()):
-                if count != 0:
-                    print("{}: {}".format(code, count))
-
-except Exception as err:
-    pass
-
-finally:
-    # Print the final total file size and status code counts
-    print("Total File Size: {}".format(total_file_size))
-    for code, count in sorted(status_code_counts.items()):
-        if count != 0:
-            print("{}: {}".format(code, count))
+if __name__ == '__main__':
+    run_log_parser()
